8c8c2d596d67842094fe7917e4c821c241cd7018
Mohamed Bakr <bakr.engineer@gmail.com>
2021-05-23 19:48:45 +0200

    add static pages for routing like posts & about and home page

---
+++ pages/posts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="posts-page" />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
