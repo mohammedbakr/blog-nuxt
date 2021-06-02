6756838fb707a908f48fc2ea37da1c33afcb5131
Mohamed Bakr <bakr.engineer@gmail.com>
2021-06-02 03:29:17 +0200

    Updates

--- _nuxt/4b8bc42.js
+++ _nuxt/4b8bc42.js
@@ -1,1 +1,144 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{276:function(t,n,o){var content=o(288);content.__esModule&&(content=content.default),"string"==typeof content&&(content=[[t.i,content,""]]),content.locals&&(t.exports=content.locals);(0,o(22).default)("0a6b9102",content,!0,{sourceMap:!1})},287:function(t,n,o){"use strict";o(276)},288:function(t,n,o){var e=o(21)(!1);e.push([t.i,".admin-auth-page[data-v-7d51da74]{padding:20px}.auth-container[data-v-7d51da74]{border:1px solid #ccc;border-radius:5px;box-shadow:0 2px 2px #ccc;width:300px;margin:auto;padding:10px;box-sizing:border-box}",""]),t.exports=e},301:function(t,n,o){"use strict";o.r(n);var e={name:"AdminAuthPage",data:function(){return{isLogin:!0,email:"",password:""}},methods:{onSubmit:function(){var t=this,n={email:this.email,password:this.password,returnSecureToken:!0};this.$store.dispatch("auth/createAndLogin",{user:n,isLogin:this.isLogin}).then((function(){return t.$router.push("/admin")}))}}},r=(o(287),o(7)),component=Object(r.a)(e,(function(){var t=this,n=t.$createElement,o=t._self._c||n;return o("div",{staticClass:"admin-auth-page"},[o("div",{staticClass:"auth-container"},[o("form",{on:{submit:function(n){return n.preventDefault(),t.onSubmit(n)}}},[o("app-control-input",{attrs:{type:"email"},model:{value:t.email,callback:function(n){t.email=n},expression:"email"}},[t._v("\n        E-Mail Address\n      ")]),t._v(" "),o("app-control-input",{attrs:{type:"password"},model:{value:t.password,callback:function(n){t.password=n},expression:"password"}},[t._v("\n        Password\n      ")]),t._v(" "),o("app-button",{attrs:{type:"submit"}},[t._v("\n        "+t._s(t.isLogin?"Login":"Sign Up")+"\n      ")]),t._v(" "),o("app-button",{staticStyle:{"margin-left":"10px"},attrs:{type:"button","btn-style":"inverted"},on:{click:function(n){t.isLogin=!t.isLogin}}},[t._v("\n        Switch to "+t._s(t.isLogin?"Signup":"Login")+"\n      ")])],1)])])}),[],!1,null,"7d51da74",null);n.default=component.exports}}]);
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-sequences */
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [5],
+  {
+    276 (t, n, o) {
+      let content = o(288)
+      content.__esModule && (content = content.default),
+      typeof content === 'string' && (content = [[t.i, content, '']]),
+      content.locals && (t.exports = content.locals);
+      (0, o(22).default)('0a6b9102', content, !0, { sourceMap: !1 })
+    },
+    287 (t, n, o) {
+      'use strict'
+      o(276)
+    },
+    288 (t, n, o) {
+      const e = o(21)(!1)
+      e.push([
+        t.i,
+        '.admin-auth-page[data-v-7d51da74]{padding:20px}.auth-container[data-v-7d51da74]{border:1px solid #ccc;border-radius:5px;box-shadow:0 2px 2px #ccc;width:300px;margin:auto;padding:10px;box-sizing:border-box}',
+        ''
+      ]),
+      (t.exports = e)
+    },
+    301 (t, n, o) {
+      'use strict'
+      o.r(n)
+      const e = {
+        name: 'AdminAuthPage',
+        data () {
+          return { isLogin: !0, email: '', password: '' }
+        },
+        methods: {
+          onSubmit () {
+            const t = this
+            const n = {
+              email: this.email,
+              password: this.password,
+              returnSecureToken: !0
+            }
+            this.$store
+              .dispatch('auth/createAndLogin', {
+                user: n,
+                isLogin: this.isLogin
+              })
+              .then(function () {
+                return t.$router.push('/admin')
+              })
+          }
+        }
+      }
+      const r = (o(287), o(7))
+      const component = Object(r.a)(
+        e,
+        function () {
+          const t = this
+          const n = t.$createElement
+          const o = t._self._c || n
+          return o('div', { staticClass: 'admin-auth-page' }, [
+            o('div', { staticClass: 'auth-container' }, [
+              o(
+                'form',
+                {
+                  on: {
+                    submit (n) {
+                      return n.preventDefault(), t.onSubmit(n)
+                    }
+                  }
+                },
+                [
+                  o(
+                    'app-control-input',
+                    {
+                      attrs: { type: 'email' },
+                      model: {
+                        value: t.email,
+                        callback (n) {
+                          t.email = n
+                        },
+                        expression: 'email'
+                      }
+                    },
+                    [t._v('\n        E-Mail Address\n      ')]
+                  ),
+                  t._v(' '),
+                  o(
+                    'app-control-input',
+                    {
+                      attrs: { type: 'password' },
+                      model: {
+                        value: t.password,
+                        callback (n) {
+                          t.password = n
+                        },
+                        expression: 'password'
+                      }
+                    },
+                    [t._v('\n        Password\n      ')]
+                  ),
+                  t._v(' '),
+                  o('app-button', { attrs: { type: 'submit' } }, [
+                    t._v(
+                      '\n        ' +
+                          t._s(t.isLogin ? 'Login' : 'Sign Up') +
+                          '\n      '
+                    )
+                  ]),
+                  t._v(' '),
+                  o(
+                    'app-button',
+                    {
+                      staticStyle: { 'margin-left': '10px' },
+                      attrs: { type: 'button', 'btn-style': 'inverted' },
+                      on: {
+                        click (n) {
+                          t.isLogin = !t.isLogin
+                        }
+                      }
+                    },
+                    [
+                      t._v(
+                        '\n        Switch to ' +
+                            t._s(t.isLogin ? 'Signup' : 'Login') +
+                            '\n      '
+                      )
+                    ]
+                  )
+                ],
+                1
+              )
+            ])
+          ])
+        },
+        [],
+        !1,
+        null,
+        '7d51da74',
+        null
+      )
+      n.default = component.exports
+    }
+  }
+])
--- _nuxt/7f877cf.js
+++ _nuxt/7f877cf.js
@@ -1,1 +1,134 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{274:function(t,e,o){var content=o(284);content.__esModule&&(content=content.default),"string"==typeof content&&(content=[[t.i,content,""]]),content.locals&&(t.exports=content.locals);(0,o(22).default)("7dba8507",content,!0,{sourceMap:!1})},283:function(t,e,o){"use strict";o(274)},284:function(t,e,o){var n=o(21)(!1);n.push([t.i,".admin-page[data-v-68de82cc]{padding:20px}.new-post[data-v-68de82cc]{border-bottom:2px solid #ccc;padding-bottom:10px}.existing-posts h1[data-v-68de82cc],.new-post[data-v-68de82cc]{text-align:center}",""]),t.exports=n},299:function(t,e,o){"use strict";o.r(e);o(32),o(23),o(27),o(42),o(26),o(43);var n=o(14),r=o(66);function c(object,t){var e=Object.keys(object);if(Object.getOwnPropertySymbols){var o=Object.getOwnPropertySymbols(object);t&&(o=o.filter((function(t){return Object.getOwnPropertyDescriptor(object,t).enumerable}))),e.push.apply(e,o)}return e}var d={middleware:["check-auth","auth"],computed:function(t){for(var i=1;i<arguments.length;i++){var source=null!=arguments[i]?arguments[i]:{};i%2?c(Object(source),!0).forEach((function(e){Object(n.a)(t,e,source[e])})):Object.getOwnPropertyDescriptors?Object.defineProperties(t,Object.getOwnPropertyDescriptors(source)):c(Object(source)).forEach((function(e){Object.defineProperty(t,e,Object.getOwnPropertyDescriptor(source,e))}))}return t}({},Object(r.b)({posts:"post/posts"})),methods:{onLogout:function(){this.$store.dispatch("auth/logout"),this.$router.push("/admin/auth")}}},l=(o(283),o(7)),component=Object(l.a)(d,(function(){var t=this,e=t.$createElement,o=t._self._c||e;return o("div",{staticClass:"admin-page"},[o("section",{staticClass:"new-post"},[o("app-button",{attrs:{type:"button"},on:{click:function(e){return t.$router.push("/admin/new-post")}}},[t._v("\n      Create post\n    ")]),t._v(" "),o("app-button",{staticStyle:{"margin-left":"10px"},attrs:{type:"button"},on:{click:t.onLogout}},[t._v("\n      Log out\n    ")])],1),t._v(" "),o("section",{staticClass:"existing-posts"},[o("h1",[t._v("Existing Posts")]),t._v(" "),o("post-list",{attrs:{"is-admin":"",posts:t.posts}})],1)])}),[],!1,null,"68de82cc",null);e.default=component.exports}}]);
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-sequences */
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [6],
+  {
+    274 (t, e, o) {
+      let content = o(284)
+      content.__esModule && (content = content.default),
+      typeof content === 'string' && (content = [[t.i, content, '']]),
+      content.locals && (t.exports = content.locals);
+      (0, o(22).default)('7dba8507', content, !0, { sourceMap: !1 })
+    },
+    283 (t, e, o) {
+      'use strict'
+      o(274)
+    },
+    284 (t, e, o) {
+      const n = o(21)(!1)
+      n.push([
+        t.i,
+        '.admin-page[data-v-68de82cc]{padding:20px}.new-post[data-v-68de82cc]{border-bottom:2px solid #ccc;padding-bottom:10px}.existing-posts h1[data-v-68de82cc],.new-post[data-v-68de82cc]{text-align:center}',
+        ''
+      ]),
+      (t.exports = n)
+    },
+    299 (t, e, o) {
+      'use strict'
+      o.r(e)
+      o(32), o(23), o(27), o(42), o(26), o(43)
+      const n = o(14)
+      const r = o(66)
+      function c (object, t) {
+        const e = Object.keys(object)
+        if (Object.getOwnPropertySymbols) {
+          let o = Object.getOwnPropertySymbols(object)
+          t &&
+            (o = o.filter(function (t) {
+              return Object.getOwnPropertyDescriptor(object, t).enumerable
+            })),
+          e.push.apply(e, o)
+        }
+        return e
+      }
+      const d = {
+        middleware: ['check-auth', 'auth'],
+        computed: (function (t) {
+          for (let i = 1; i < arguments.length; i++) {
+            const source = arguments[i] != null ? arguments[i] : {}
+            i % 2
+              ? c(Object(source), !0).forEach(function (e) {
+                Object(n.a)(t, e, source[e])
+              })
+              : Object.getOwnPropertyDescriptors
+                ? Object.defineProperties(
+                  t,
+                  Object.getOwnPropertyDescriptors(source)
+                )
+                : c(Object(source)).forEach(function (e) {
+                  Object.defineProperty(
+                    t,
+                    e,
+                    Object.getOwnPropertyDescriptor(source, e)
+                  )
+                })
+          }
+          return t
+        })({}, Object(r.b)({ posts: 'post/posts' })),
+        methods: {
+          onLogout () {
+            this.$store.dispatch('auth/logout'),
+            this.$router.push('/admin/auth')
+          }
+        }
+      }
+      const l = (o(283), o(7))
+      const component = Object(l.a)(
+        d,
+        function () {
+          const t = this
+          const e = t.$createElement
+          const o = t._self._c || e
+          return o('div', { staticClass: 'admin-page' }, [
+            o(
+              'section',
+              { staticClass: 'new-post' },
+              [
+                o(
+                  'app-button',
+                  {
+                    attrs: { type: 'button' },
+                    on: {
+                      click (e) {
+                        return t.$router.push('/admin/new-post')
+                      }
+                    }
+                  },
+                  [t._v('\n      Create post\n    ')]
+                ),
+                t._v(' '),
+                o(
+                  'app-button',
+                  {
+                    staticStyle: { 'margin-left': '10px' },
+                    attrs: { type: 'button' },
+                    on: { click: t.onLogout }
+                  },
+                  [t._v('\n      Log out\n    ')]
+                )
+              ],
+              1
+            ),
+            t._v(' '),
+            o(
+              'section',
+              { staticClass: 'existing-posts' },
+              [
+                o('h1', [t._v('Existing Posts')]),
+                t._v(' '),
+                o('post-list', { attrs: { 'is-admin': '', posts: t.posts } })
+              ],
+              1
+            )
+          ])
+        },
+        [],
+        !1,
+        null,
+        '68de82cc',
+        null
+      )
+      e.default = component.exports
+    }
+  }
+])
